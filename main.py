3e20e758e8363502c076c91c38cbcb0e3d21bb46
Mehmet Utku "Alphard" Öztürk <98752107+thealphard@users.noreply.github.com>
2023-07-16 17:20:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pandas as pd
 from math import sqrt
 import csv
 sex = input("Are you a [w]oman or [m]an?")
-dir_men = r"C:\Users\gsutk\Desktop\Projects\LoveSolutions\men.csv"
-dir_women = r"C:\Users\gsutk\Desktop\Projects\LoveSolutions\women.csv"
+dir_men = "men.csv"
+dir_women = "women.csv"
 wdir = r""
 if(sex.lower()=="w"):
     data = pd.read_csv(dir_men)
@@ -14,7 +14,7 @@ elif(sex.lower()=="m"):
     wdir = dir_men
 new_value = []
 topfive = []
-print("In the next steps, your age and interests will be asked. No need to mention the obvious, for age part you will type your age. For the each interest you'll be asked, you must rate your level of interest from 1 to 100.")
+print("In the next steps, your age and interests will be asked. For age, you have to type your age. For the each interest you'll be asked, you must rate your level of interest from 1 to 100.")
 k = 5
 characteristics = len(data.columns) - 1
 distances = {}
@@ -22,6 +22,12 @@ for i in data.columns[0 : characteristics]:
     new_value.append(float(input(f"Type your {i}.")))
 for index, row in data.iterrows():
     distance = 0
+
+    if(new_value[0] < 18 and row[0] > 17):
+        continue
+    if(new_value[0] > 17 and row[0] < 17):
+        continue
+# We are avoiding matching an adult with a minor
     for val in range(characteristics):
         distance += (row[data.columns[val]]-new_value[val])**2
     distance = sqrt(distance)
